3e460b6d91bb73dfcd86f637fa2ff4babda909cd
zkbingo-bx <zkbingo-bx@proton.me>
2024-08-14 21:39:05 +0800

    FFFFF Fix command

--- scripts/replace-contracts.sh
+++ scripts/replace-contracts.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # replace-contracts.sh
 cp -r ./v1.4.1 ./node_modules/@safe-global/safe-deployments/dist/assets
 cp -r ./v1.3.0 ./node_modules/@safe-global/safe-deployments/dist/assets
